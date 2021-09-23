038049b87b3fccad944782c3c3536832da5ad3a1
Leandro <play3rtox1c@gmail.com>
2021-09-23 09:20:52 -0300

    colocando como titulo os generos

--- src/components/ResultOfSearch.jsx
+++ src/components/ResultOfSearch.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import getSearch from "../Apis_requests/getSearch";
 import getTrending from "../Apis_requests/getTrending";
 import MovieCard from "./movieCard";
+import getGenrers from '../Apis_requests/getGenrers'
 import './resultOfSearch.css';
 
 class ResultOfSearch extends Component {
@@ -87,9 +88,17 @@ class ResultOfSearch extends Component {
       ));
     }
 
+    let titulo;
+    if (types !== 'All') {
+      console.log(types)
+      titulo = getGenrers.find((item) => item[0] === parseInt(types))[1]
+    } else {
+      titulo = query
+    }
+
     return (
       <div className="content-grid">
-        <h1 className="titulo-busca">Busca por { query }:</h1>
+        <h1 className="titulo-busca">Busca por { titulo }:</h1>
         <section className="result-search-grid">
           {componentes}
           <button className="bot-add-new-films" onClick={this.addItem}><svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="plus-square" class="svg-inline--fa fa-plus-square fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M352 240v32c0 6.6-5.4 12-12 12h-88v88c0 6.6-5.4 12-12 12h-32c-6.6 0-12-5.4-12-12v-88h-88c-6.6 0-12-5.4-12-12v-32c0-6.6 5.4-12 12-12h88v-88c0-6.6 5.4-12 12-12h32c6.6 0 12 5.4 12 12v88h88c6.6 0 12 5.4 12 12zm96-160v352c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V80c0-26.5 21.5-48 48-48h352c26.5 0 48 21.5 48 48zm-48 346V86c0-3.3-2.7-6-6-6H54c-3.3 0-6 2.7-6 6v340c0 3.3 2.7 6 6 6h340c3.3 0 6-2.7 6-6z"></path></svg></button>
